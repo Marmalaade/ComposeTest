3a0932e7d914e33861c0c3474f0c5825a036eef2
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2023-09-03 23:34:02 +0300

    test: UI/1

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,104 +1,74 @@
 package com.example.composetest
 
-import android.media.midi.MidiOutputPort
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainList()
-            // MainBox()
+            LoadData()
         }
     }
 }
 
 
 @Composable
-fun MainList() {
+fun LoadData() {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-    ) {
-        FirstSection()
-    }
+            .fillMaxSize(1f),
+        contentAlignment = Alignment.Center
 
+    ) {
+        val isLoading = remember {
+            mutableStateOf(false)
+        }
+        Button(
+            onClick = {
+                isLoading.value = !isLoading.value
+            }, colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Magenta,
+                contentColor = Color.Green
+            )
+        ) {
+            if (!isLoading.value) {
+                Text(text = "Data loading")
+            } else {
+                CircularProgressIndicator(
+                    color = Color.White, modifier = Modifier
+                        .height(15.dp)
+                        .width(20.dp)
+                )
+            }
+        }
+
+    }
 }
 
 @Composable
-fun FirstSection() {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth(1f)
-            .padding(16.dp)
-    ) {
-        Column(verticalArrangement = Arrangement.Center) {
-            Text(modifier = Modifier.background(Color.Red), text = "Good Morning")
-            Text(modifier = Modifier.background(Color.LightGray), text = "How are you?")
-        }
-        Icon(painter = painterResource(id = R.drawable.search), contentDescription = "search_icon")
-
-    }
-
-
+fun LoadImage(path: String) {
+    GlideImage
 }
 
-@Composable
-fun MainBox() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.1f)
-            .padding(16.dp),
-        backgroundColor = Color.LightGray,
-        shape = RoundedCornerShape(10.dp),
-        elevation = 6.dp
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(
-                painter = painterResource(id = R.drawable.search),
-                contentDescription = "search_image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(24.dp)
-                    .align(Alignment.CenterVertically)
-                    .offset(8.dp)
 
-            )
-            Text(
-                text = "Должность, вакансия, ключевые слова...",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .offset(16.dp)
-            )
-        }
-    }
-}
 
 
 
---
+++ app/src/main/java/com/example/composetest/utils/ImageLoader.kt
@@ -0,0 +1,8 @@
+package com.example.composetest.utils
+
+class ImageData {
+    companion object {
+        private const val IMAGE_URL = "https://rickandmortyapi.com/api/character/avatar/228.jpeg"
+    }
+
+}
